de3d9ae3fcc201b608b4bc9f2fd507d0920f3cef
Vipul Dessai <badgamerbad@gmail.com>
2025-02-15 03:18:54 +0530

    init

---
+++ Source/UdmyOSubSysCppUSrc/UdmyOSubSysCppUSrc.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class UdmyOSubSysCppUSrc : ModuleRules
+{
+	public UdmyOSubSysCppUSrc(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "OnlineSubsystemSteam", "OnlineSubsystem" });
+	}
+}
---
+++ Source/UdmyOSubSysCppUSrc/UdmyOSubSysCppUSrcGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UdmyOSubSysCppUSrcGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AUdmyOSubSysCppUSrcGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AUdmyOSubSysCppUSrcGameMode();
+};
+
+
+
